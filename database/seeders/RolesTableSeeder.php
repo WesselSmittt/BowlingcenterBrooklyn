defcd65b1b5dde9ffbdd840a946309a2738bdcb2
Dylankm86 <335478@student.mobutrecht.nl>
2024-04-05 16:08:37 +0200

    creating a role table for auth of the medewerker

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            ['name' => 'klant'],
+            ['name' => 'medewerker'],
+        ]);
+    }
+}
