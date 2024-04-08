b27b3909a4e221150614f8be22c4747d8ad20374
Dylankm86 <335478@student.mobutrecht.nl>
2024-04-08 10:34:56 +0200

    added the personen table to the database and seeded it

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(TariffsTableSeeder::class);
+        // $this->call(TariffsTableSeeder::class);
 
         // \App\Models\User::factory(10)->create();
 
