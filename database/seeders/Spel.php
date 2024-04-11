1b922fa1b65e10c816c5ca1081674e80b349f4e6
timdegroot05 <336795@student.mboutrecht.nl>
2024-04-11 21:10:08 +0200

    heb de seeders nog effe toegevoegd voor mijn tabellen zodat iedereen wat informatie op de pagina kan zien

--- database/seeders/Spel.php
+++ database/seeders/Spel.php
@@ -14,7 +14,36 @@ class Spel extends Seeder
     {
         DB::table('spel')->insert([
             [
-                
+                'PersoonId' => 1,
+                'ReserveringId' => 1,
+            ],
+            [
+                'PersoonId' => 2,
+                'ReserveringId' => 2,
+            ],
+            [
+                'PersoonId' => 3,
+                'ReserveringId' => 3,
+            ],
+            [
+                'PersoonId' => 1,
+                'ReserveringId' => 4,
+            ],
+            [
+                'PersoonId' => 4,
+                'ReserveringId' => 5,
+            ],
+            [
+                'PersoonId' => 6,
+                'ReserveringId' => 4,
+            ],
+            [
+                'PersoonId' => 7,
+                'ReserveringId' => 4,
+            ],
+            [
+                'PersoonId' => 8,
+                'ReserveringId' => 4,
             ]
         ]);
     }
--- database/seeders/Uitslag.php
+++ database/seeders/Uitslag.php
@@ -12,6 +12,39 @@ class Uitslag extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('uitslag')->insert([
+            [
+                'SpelId' => 1,
+                'AantalPunten' => 277,
+            ],
+            [
+                'SpelId' => 2,
+                'AantalPunten' => 284,
+            ],
+            [
+                'SpelId' => 3,
+                'AantalPunten' => 120,
+            ],
+            [
+                'SpelId' => 4,
+                'AantalPunten' => 34,
+            ],
+            [
+                'SpelId' => 5,
+                'AantalPunten' => 42,
+            ],
+            [
+                'SpelId' => 6,
+                'AantalPunten' => 234,
+            ],
+            [
+                'SpelId' => 7,
+                'AantalPunten' => 2,
+            ],
+            [
+                'SpelId' => 8,
+                'AantalPunten' => 255,
+            ]
+        ]);
     }
 }
