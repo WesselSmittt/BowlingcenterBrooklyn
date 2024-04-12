c07216a7773713f5b079b753180d8100c6b88a28
Dylankm86 <335478@student.mobutrecht.nl>
2024-04-12 12:34:09 +0200

    Refactor DatabaseSeeder.php to add new seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,18 @@ class DatabaseSeeder extends Seeder
          */
         public function run(): void
         {
-                $this->call(TariffsTableSeeder::class);
+                $this->call(Pakketoptie::class);
+                $this->call(Persoon::class);
+                $this->call(Reserveringstatus::class);
+                $this->call(Reservering::class);
+                $this->call(ScoreTableSeeder::class);
+                $this->call(GameTableSeeder::class);
+                $this->call(PlayerTableSeeder::class);
+                $this->call(ReservationTableSeeder::class);
+                $this->call(UserTableSeeder::class);
+                $this->call(TariffTableSeeder::class);
+                $this->call(RolesTableSeeder::class);
+                // Add more seeders as needed
 
                 // \App\Models\User::factory(10)->create();
 
